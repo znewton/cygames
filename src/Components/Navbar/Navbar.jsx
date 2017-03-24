9b7145ae3c08b4abed0f8c1a1fca956854bcf2d3
Zach Newton <znewton@iastate.edu>
2017-03-24 10:53:49 -0500

    Some Navbar work

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -8,10 +8,13 @@ export default class Navbar extends Component {
 		return (
 			<nav className="Navbar">
 				<div className="navbar-left">
+					<i className="fa fa-gamepad" title="Open Games Menu"/>
+				</div>
+				<div className="navbar-middle">
 					<span>CyGames</span>
 				</div>
 				<div className="navbar-right">
-
+					<i className="fa fa-comments" title="Open Chat Bar"/>
 				</div>
 			</nav>
 		);
