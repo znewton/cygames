a2e8a9d663b73054adde5723b881ba0a4611c52b
Nick Behrens <behrensn@iastate.edu>
2017-03-24 02:49:08 -0500

    Added firebase starting point

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ const port = process.env.PORT || 3000;
 const app = express();
 const server = app.listen(port);
 const io = require('socket.io')(server);
+const firebase = require('firebase');
+
+var config = {
+	apiKey: "AIzaSyA1fhVzgiiYt27zj98FabJN-fKGp4ioMCY",
+	authDomain: "cygames-c3548.firebaseapp.com",
+	databaseURL: "https://cygames-c3548.firebaseio.com",
+	storageBucket: "cygames-c3548.appspot.com",
+	messagingSenderId: "175107300140"
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+var increment = 0;
 
 // serve static assets normally
 app.use(express.static(__dirname + '/public'));
@@ -20,13 +34,55 @@ app.get('*', function (request, response){
 console.log("server started on port " + port);
 
 io.on('connection', function(socket){
-	console.log("new connection");
+	getAllMessages();
 
-	socket.on('hello',function(msg){
-		console.log("Hello there little one..");
+	socket.on('hello',function () {
+		console.log("Hello there litte one");
 	});
-})
 
-opn('http://localhost:'+port);
+	socket.on('startSession',function(msg){
+		socket.userName = msg.userName;
+		writeUserData(socket.userName);
+		increment++;
+		sayHello();
+	});
+
+	socket.on('chat message', function(msg){
+			var messageString = msg.toString();
+			var message = socket.userName +":"+msg;
+			firebase.database().ref('message/m'+increment).set({
+					msg: msg,
+					sender: socket.userName
+			});
+			increment++;
+			socket.emit('chat message',message);
+			socket.broadcast.emit('chat message',message);
+	});
+
+	socket.on('disconnect', function(){
+			console.log('user disconnected');
+			firebase.database().ref('users/'+socket.id).remove();
+	});
+
+	function getAllMessages() {
+		firebase.database().ref('message/').on('value',function(snapshot){
+			console.log("Snapshot is: "+snapshot.val());
+			for(var object in snapshot.val()){
+				console.log(object.sender);
+			}
+		});
+	}
+
+	function writeUserData(name) {
+  firebase.database().ref('users/' + name).set({
+			name: name
+  });
+}
+	function sayHello(){
+		socket.emit('helloClient',{ username: socket.userName });
+	}
+});
+
+//opn('http://localhost:'+port);
 
 opn('http://localhost:'+port+"/testing");
--- public/testingHTML/default.js
+++ public/testingHTML/default.js
@@ -1,2 +1,17 @@
 const socket = io();
+
 socket.emit("hello");
+socket.emit("startSession", {userName: "John"});
+socket.on("helloClient",function(data){
+  console.log(data);
+});
+
+$('form').submit(function(){
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+    return false;
+});
+
+socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').text(msg));
+});
