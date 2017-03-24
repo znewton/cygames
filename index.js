6aed234bd5eeec32d9c8064fb1ba16d6e7424ca1
Nick Behrens <behrensn@iastate.edu>
2017-03-24 00:35:18 -0500

    Added testing files

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const path = require('path');
 const opn = require('opn');
 const port = process.env.PORT || 3000;
 const app = express();
-const socket = require('socket.io');
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 // serve static assets normally
 app.use(express.static(__dirname + '/public'));
 
 // Handles all routes so you do not get a not found error
+app.get('/testing', function (request, response){
+	response.sendFile(path.resolve(__dirname, 'public/testingHTML', 'index.html'))
+});
 app.get('*', function (request, response){
 	response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 });
@@ -17,3 +21,7 @@ app.listen(port);
 console.log("server started on port " + port);
 
 opn('http://localhost:'+port);
+
+io.on('connection', function(socket){
+	console.log("new connection");
+})
