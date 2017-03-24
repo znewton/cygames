505b90d3e07dc8e1ece0e7e49ade272dce1ccbdf
Zach Newton <znewton@iastate.edu>
2017-03-24 13:16:17 -0500

    More navbar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
 import './Sass/App.scss';
 //Component imports
-import Basic from './Components/Basic/Basic.jsx';
 import Navbar from './Components/Navbar/Navbar.jsx';
 
 export default class App extends Component {
 	constructor() {
 		super();
-		this.state = {msg: 'Hello World'}
+		this.state = {
+			msg: 'Hello World'
+		};
 	}
 	render() {
 		return (
 			<div className="App">
 				<Navbar/>
-				<h1>{this.state.msg}</h1>
-				<Basic/>
 			</div>
 		);
 	}
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,18 +3,31 @@ import React, { Component } from 'react';
 export default class Navbar extends Component {
 	constructor() {
 		super();
+		this.state = {
+			gameMenuOpen: false,
+			chatBarOpen: false,
+		};
+	}
+	handleGameMenuOpen(){
+
 	}
 	render() {
 		return (
 			<nav className="Navbar">
 				<div className="navbar-left">
-					<i className="fa fa-gamepad" title="Open Games Menu"/>
+					<span className={"menu-btn"+(this.state.gameMenuOpen ? ' open' : '')}>
+						<i className="fa fa-gamepad fa-fw"
+							 title="Open Games Menu"/>
+					</span>
 				</div>
 				<div className="navbar-middle">
-					<span>CyGames</span>
+					<h2>CyGames</h2>
 				</div>
 				<div className="navbar-right">
-					<i className="fa fa-comments" title="Open Chat Bar"/>
+					<span className={"menu-btn"+(this.state.chatBarOpen ? ' open' : '')}>
+						<i className="fa fa-comments fa-fw"
+							 title="Open Chat Bar"/>
+					</span>
 				</div>
 			</nav>
 		);
