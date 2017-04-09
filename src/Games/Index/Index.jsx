194696a0c6c35567412110310a3a23073ba9022b
znewton <znewton@iastate.edu>
2017-04-09 01:43:44 -0500

    Some styling

--- src/Games/Index/Index.jsx
+++ src/Games/Index/Index.jsx
@@ -8,9 +8,16 @@ export default class Index extends Component {
 	render() {
 		return (
 			<section className="Index">
-				<h1>Welcome to CyGames!</h1>
-				<p>To play a game, select one from the games bar on the left.</p>
-				<p>To chat, open up the chat bar on the right!</p>
+				<div className="container">
+					<div className="segment play">
+						<i className="fa fa-gamepad" />
+						<span>To play a game, select one from the games bar on the left.</span>
+					</div>
+					<div className="segment chat">
+						<span>To chat, open up the chat bar on the right!</span>
+						<i className="fa fa-comments" />
+					</div>
+				</div>
 			</section>
 		);
 	}
